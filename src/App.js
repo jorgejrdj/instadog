c8dfeb0ecf2cbce99b2a764e39d035d8a9d4d560
Jorge de Souza <jorgejrdj1@gmail.com>
2021-07-09 19:52:04 -0300

    Router Home ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Api from './api/Api';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Footer } from './Components/Footer';
+import { Header } from './Components/Header';
+import { Home } from './Components/Home';
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
