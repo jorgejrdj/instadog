7c6ba46fb425598d2c554e87820f64adc4bd199c
Jorge de Souza <jorgejrdj1@gmail.com>
2021-07-09 20:10:00 -0300

    Estrutura Inicial finalizada

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-      Header
+      <nav className="container">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Criar conta</Link>
+      </nav>
     </div>
   )
 }
