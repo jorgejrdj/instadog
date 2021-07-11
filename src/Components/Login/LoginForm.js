ddd186e19f127ce10c5ce2eb714eafde9a0a1345
Jorge de Souza <jorgejrdj1@gmail.com>
2021-07-11 11:03:16 -0300

    USeContext

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { TOKEN_POST, USER_GET } from '../../api';
 import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../UserContext';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 
@@ -9,48 +9,28 @@ export const LoginForm = () => {
   const username = useForm('email');
   const password = useForm();
 
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token) {
-      getUser(token);
-    }
-  }, []);
 
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
-
+  const { userLogin } = React.useContext(UserContext)
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token)
+      userLogin(username.value, password.value)
     }
+
+
+    return (
+      <section>
+        <h1>Login</h1>
+        <form action="" onSubmit={handleSubmit}>
+          <Input label="Usuário" type="text" name="username" {...username} />
+          <Input label="Senha" type="password" name="password" {...password} />
+          <Button> Entrar </Button>
+        </form>
+
+        <Link to="/login/criar">Cadastrar-se</Link>
+      </section>
+    )
   }
-
-
-  return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" {...username} />
-        <Input label="Senha" type="password" name="password" {...password} />
-        <Button> Entrar </Button>
-      </form>
-
-      <Link to="/login/criar">Cadastrar-se</Link>
-    </section>
-  )
 }
