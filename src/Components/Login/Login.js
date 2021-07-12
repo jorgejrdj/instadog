be143450a2362b70c64c1f59823ea794152d818d
Jorge de Souza <jorgejrdj1@gmail.com>
2021-07-12 20:00:30 -0300

    login finalizado

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
 import { LoginCreate } from './LoginCreate'
 import { LoginForm } from './LoginForm'
 import { LoginPasswordLost } from './LoginPasswordLost'
 import { LoginPasswordReset } from './LoginPasswordReset'
 
 export const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/criar" element={<LoginCreate />} />
-        <Route path="/perdeu" element={<LoginPasswordLost />} />
-        <Route path="/resetar" element={<LoginPasswordReset />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -6,31 +6,34 @@ import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 
 export const LoginForm = () => {
-  const username = useForm('email');
+  const username = useForm();
   const password = useForm();
 
-
-  const { userLogin } = React.useContext(UserContext)
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      userLogin(username.value, password.value)
+      userLogin(username.value, password.value);
     }
-
-
-    return (
-      <section>
-        <h1>Login</h1>
-        <form action="" onSubmit={handleSubmit}>
-          <Input label="Usuário" type="text" name="username" {...username} />
-          <Input label="Senha" type="password" name="password" {...password} />
-          <Button> Entrar </Button>
-        </form>
-
-        <Link to="/login/criar">Cadastrar-se</Link>
-      </section>
-    )
   }
-}
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {error && <p>{error}</p>}
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
